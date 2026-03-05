b8d691860b2880ec93334fccbcfeb849512565ec
taim-sul <taim@smartlogistics.dk>
2026-03-05 21:42:06 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,31 @@
+var CACHE = 'tidsreg-v1';
+var FILES = [
+  '/Pilot-tidsregistrering/medarbejdertidsregistrering.html',
+  '/Pilot-tidsregistrering/manifest.json'
+];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE).then(function(cache) {
+      return cache.addAll(FILES);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(keys.filter(function(k){return k!==CACHE;}).map(function(k){return caches.delete(k);}));
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    fetch(e.request).catch(function() {
+      return caches.match(e.request);
+    })
+  );
+});

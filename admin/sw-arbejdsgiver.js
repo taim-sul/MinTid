c9f91721a8179d531fea07612b0e3c2f0f0db96a
taim-sul <taim@smartlogistics.dk>
2026-03-24 13:36:38 +0100

    Add files via upload

--- admin/sw-arbejdsgiver.js
+++ admin/sw-arbejdsgiver.js
@@ -1,27 +1,14 @@
-var CACHE = 'MinTid-Arbejdsgiver-v4';
-
-self.addEventListener('install', function(e) {
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', function(e) {
-  e.waitUntil(
-    caches.keys().then(function(keys){
-      return Promise.all(keys.filter(function(k){return k!==CACHE;}).map(function(k){return caches.delete(k);}));
-    })
-  );
+var CACHE = 'MinTid-Arbejdsgiver-v5';
+self.addEventListener('install', function(e){self.skipWaiting();});
+self.addEventListener('activate', function(e){
+  e.waitUntil(caches.keys().then(function(keys){
+    return Promise.all(keys.filter(function(k){return k!==CACHE;}).map(function(k){return caches.delete(k);}));
+  }));
   self.clients.claim();
 });
-
-self.addEventListener('fetch', function(e) {
-  if(e.request.method !== 'GET') return;
-  e.respondWith(
-    fetch(e.request).then(function(response){
-      var clone = response.clone();
-      caches.open(CACHE).then(function(cache){ cache.put(e.request, clone); });
-      return response;
-    }).catch(function(){
-      return caches.match(e.request);
-    })
-  );
+self.addEventListener('fetch', function(e){
+  if(e.request.method!=='GET')return;
+  e.respondWith(fetch(e.request).then(function(r){
+    var c=r.clone();caches.open(CACHE).then(function(cache){cache.put(e.request,c);});return r;
+  }).catch(function(){return caches.match(e.request);}));
 });

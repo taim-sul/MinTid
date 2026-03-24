c15060ec0e8b31c87515594ded0ff301193178b8
taim-sul <taim@smartlogistics.dk>
2026-03-24 12:30:44 +0100

    Add files via upload

--- admin/sw.js
+++ admin/sw.js
@@ -1,4 +1,4 @@
-var CACHE = 'admin-v1';
+var CACHE = 'admin-v5';
 self.addEventListener('install', function(e){self.skipWaiting();});
 self.addEventListener('activate', function(e){
   e.waitUntil(caches.keys().then(function(keys){
